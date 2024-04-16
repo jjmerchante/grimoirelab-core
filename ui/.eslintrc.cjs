967f91c4f1339fbb1e19a944a4ae40f370809a2f
Eva Millán <evamillan@bitergia.com>
2024-04-16 15:34:45 +0200

    [ui] Initial configuration
    Adds the initial files needed to configure the UI.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

---
+++ ui/.eslintrc.cjs
@@ -0,0 +1,23 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
+    'plugin:storybook/recommended',
+    'plugin:storybook/recommended'
+  ],
+  overrides: [
+    {
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ ui/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar color="primary" density="compact" flat>
+      <template #prepend>
+        <img src="./assets/favicon.png" height="30" />
+      </template>
+    </v-app-bar>
+    <v-main>
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped lang="scss">
+:deep(.v-toolbar__prepend) {
+  margin-inline: 14px auto;
+}
+.v-navigation-drawer {
+  .v-list-item.v-list-item--density-compact {
+    border-radius: 4px;
+    padding-inline: 8px;
+
+    :deep(.v-list-item__spacer) {
+      width: 16px;
+    }
+  }
+  .v-list-item-title {
+    font-size: 0.875rem;
+    font-weight: 500;
+    line-height: 1.375rem;
+    letter-spacing: 0.0071428571em;
+  }
+}
+</style>
