f0ce9ec097d294cd52c9c10e92f85b59ec3fce2f
Eva Millán <evamillan@bitergia.com>
2025-02-20 16:58:10 +0100

    [ui] Filter and order tasks
    Adds tabs to filter the list of tasks by their status and
    a selector to order them by last run or schedule date.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

--- ui/src/views/Task/ListView.vue
+++ ui/src/views/Task/ListView.vue
@@ -8,6 +8,8 @@
       @delete="confirmDeleteTask($event)"
       @reschedule="rescheduleTask($event)"
       @update:page="fetchTasks($event)"
+      @update:status="fetchTasks(this.page, $event)"
+      @update:filters="fetchTasks(this.page, $event)"
     />
     <v-snackbar v-model="snackbar.open" :color="snackbar.color">
       {{ snackbar.text }}
@@ -92,9 +94,13 @@ export default {
       }
       this.dialog = false
     },
-    async fetchTasks(page = 1) {
+    async fetchTasks(page = 1, filters) {
       try {
-        const response = await API.scheduler.list({ page })
+        const params = { page }
+        if (filters) {
+          Object.assign(params, filters)
+        }
+        const response = await API.scheduler.list(params)
         if (response.data.results) {
           this.tasks = response.data.results
           this.count = response.data.count
