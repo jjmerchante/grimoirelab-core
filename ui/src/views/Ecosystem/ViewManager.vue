48e5ca2032b4d83aedb0ab1b232a773464cb7d78
Eva Millán <evamillan@bitergia.com>
2025-08-14 16:54:12 +0200

    [ui] Add ecosystems view manager
    Adds a ViewManager component to load ecosystem, project or repository views
    depending on its props.
    
    Signed-off-by: Eva Millán <evamillan@bitergia.com>

---
+++ ui/src/views/Ecosystem/ViewManager.vue
@@ -0,0 +1,56 @@
+<template>
+  <component :is="currentComponent"></component>
+</template>
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    ListView: defineAsyncComponent(() => import('./ListView.vue')),
+    ProjectListView: defineAsyncComponent(() => import('../Project/ListView.vue')),
+    ProjectView: defineAsyncComponent(() => import('../Project/DetailView.vue')),
+    RepoView: defineAsyncComponent(() => import('../Repo/DetailView.vue')),
+    NewRepoView: defineAsyncComponent(() => import('../Repo/NewRepo.vue')),
+    SBoMView: defineAsyncComponent(() => import('../Repo/LoadSbom.vue'))
+  },
+  props: {
+    ecosystem: {
+      type: String,
+      required: false,
+      default: null
+    },
+    project: {
+      type: String,
+      required: false,
+      default: null
+    },
+    repo: {
+      type: String,
+      required: false,
+      default: null
+    },
+    create: {
+      type: String,
+      required: false,
+      default: null
+    }
+  },
+  computed: {
+    currentComponent() {
+      if (!this.ecosystem) {
+        return 'ListView'
+      } else if (!this.project) {
+        return 'ProjectListView'
+      } else if (!this.repo && !this.create) {
+        return 'ProjectView'
+      } else if (this.create && this.create === 'repo') {
+        return 'NewRepoView'
+      } else if (this.create && this.create === 'sbom') {
+        return 'SBoMView'
+      } else if (this.repo) {
+        return 'RepoView'
+      }
+    }
+  }
+}
+</script>
